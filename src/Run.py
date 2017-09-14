793cce404b6ba88219fa165c0fa795148590a57c
Welperooni <7xcam7@gmail.com>
2017-09-14 20:24:28 +0200

    Added Descriptions, cleared debug code

--- src/Run.py
+++ src/Run.py
@@ -27,15 +27,21 @@ class Board:
     boardE = {}
     for x in range(1, 11):
       for y in range(1, 11):
+
+        # assigns string value to tempname, for example C4
         tempname = ABlist[x] + str(y)
+
+        # assigns given string value to these two to-be instance variables
         tempname1 = tempname
         tempname2 = tempname
+
+        # instance variables than can be invoked by the point name
         tempname1 = Point.PointU(tempname, (x, y))
         tempname2 = Point.PointE(tempname, (x, y))
+
+        # dictionaries that have "coordinate names" for keys, pointers to their class instance as values
         boardU[tempname] = tempname1
         boardE[tempname] = tempname2
-        print(boardU[tempname])
-        print(boardE[tempname])     
 
 if __name__ == '__main__':
 	main()
