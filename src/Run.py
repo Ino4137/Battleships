56a2dff591b036c3dd6ae28be193c1e3b26a6d2b
Welperooni <7xcam7@gmail.com>
2017-09-15 02:25:47 +0200

    Almost fixed placeShips method.

--- src/Run.py
+++ src/Run.py
@@ -1,5 +1,5 @@
 import Point
-import random
+import random, logging
 
 # declaration of dictionaries used for keeping track of positions on boards
 boardU = { } 
@@ -66,58 +66,72 @@ def placeShips(board):
   ID = 0
 
   # for all ships 
-  for l in range(5):
+  for l in range(4):
+
+    beg = ShipFrame[l][0] 
+    end = ShipFrame[l][1]
 
     # while number of ships placed is not 0
-    while ShipFrame[l][1] != 0:
-
+    while end != 0:
       # horizontal = 1, vertical = 0
       alignment = random.randint(0,1)
-      
-      # random starting point of the ship
-      startX = random.randint(1, 10-ShipFrame[l][0])
-      startY = random.randint(1, 10-ShipFrame[l][0])
 
-      start = [startX, startY]
+      # random starting point of the ship
+      startX = random.randint(1, 10--ShipFrame[l][0])
+      startY = random.randint(1, 10)
+
+      if alignment == 1:
+        start = [startX, startY]
+      else:
+        start = [startY, startX]
 
       # loops over all the points under the ship
       try:
-        for unit in range(ShipFrame[l][0]):
+        for unit in range(ShipFrame[l][0] + 1):
+          #print("debug.unit: " + str(unit))
+     
+          #print("debug.1")
 
           # if a point is taken 
-          if False == validatePoint(start[alignment] + ShipFrame[l][0] - 1 - unit, board):
+          if False == validatePoint(start, board):
             raise Exception
+          else:
+            beg -= 1
+            start[alignment] += unit
+
+          #print(start)
 
           # if all points returned True
-          if unit - ShipFrame[l][0] == 0:
-            print("debug.allaretrue")
+          if ShipFrame[l][0] - unit == 0:
 
             # reset the assisting list of points
             shipPoints = [ ] 
 
             # decrement remaining count
-            ShipFrame[l][1] -= 1
+            end -= 1
 
             # get a new pointer
             tempID = ID
 
             # once more loops over the points of the ship to get their instances
             for num in range(ShipFrame[l][0]):
-              for key in board.keys():
-                if tuple(start[alignment] + ShipFrame[l][0] - 1 - num) == key.XY:
-                  shipPoints.append(key.name)
-                  key.taken = True
+              for key in board:
+                if [start[alignment] + ShipFrame[l][0] - 1 - num, start[int(abs(alignment-1))]] == list(board[key].XY):
+                  print(board[key].name)
+                  shipPoints.append(board[key].name)
+                  board[key].taken = True
+                  print(shipPoints)
 
             # finally, crates the variable instance of Ship class
             tempID = Point.Ship(ID, ShipFrame[l][0], shipPoints)
-
+            print(tempID.alivePoints)
             # increment ID
             ID += 1 
-            print("debug.ID = " + str(ID))
+            #print("debug.ID = " + str(ID))
 
       # if a point returned False
-      except Exception:
-        print("debug.Exception")
+      except Exception as e:
+        logging.exception("message")
 
         # not decrementing the ship num, jumping to next loop in Run.py:69
         continue
@@ -128,6 +142,7 @@ def validatePoint(point, board):
   '''
   Checks if a given point is taken
   '''
+  (x, y) = point
   for key in board.keys():
     if board[key].XY == (x, y):
       if board[key].taken == False:
