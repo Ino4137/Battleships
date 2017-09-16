f5fa055e31606a1baf65439c1c071def47ee3c73
Welperooni <7xcam7@gmail.com>
2017-09-16 02:01:37 +0200

    Added some comments

--- src/Point.py
+++ src/Point.py
@@ -1,10 +1,21 @@
+class Ship:
+  def __init__(self, ID, length, alivePoints, shotPoints=[]):
+    self.ID = ID
+    self.length = length
+    self.alivePoints = alivePoints
+    self.shotPoints = shotPoints
+
 class Point:
-  def __init__(self, name, XY, taken=False, flagged=False, shot=False):
-    self.name = name
+  def __init__(self, name, XY, taken=False, flagged=False, shot=False, ship = None):
+    self.name = name 
     self.XY = XY
-    self.taken = taken
-    self.flagged = flagged
+    self.taken = taken # redundant, i'm only using .ship anyway
+    self.flagged = flagged # if i'm ever to implement a certain feature
     self.shot = shot
+
+    # 42 is default value, do not ask why
+    self.ship = ship
+
 
 class PointU(Point):
   pass
@@ -12,9 +23,3 @@ class PointU(Point):
 class PointE(Point):
   pass
 
-class Ship:
-  def __init__(self, ID, length, alivePoints, shotPoints=[]):
-    self.ID = ID
-    self.length = length
-    self.alivePoints = alivePoints
-    self.shotPoints = shotPoints
